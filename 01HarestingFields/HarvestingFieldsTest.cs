77c08cfae46a4cf4ca889abf5029aa04d2a3e8d8
GodSmack91 <godsmack_91@abv.bg>
2017-08-03 16:34:46 +0300

    Reflection excercise projects

---
+++ 01HarestingFields/HarvestingFieldsTest.cs
@@ -0,0 +1,45 @@
+﻿namespace _01HarestingFields
+{
+    using System;
+    using System.Linq;
+    using System.Reflection;
+
+    public class HarvestingFieldsTest
+    {
+        public static void Main(string[] args)
+        {
+            Type harvestingFieldsType = typeof(HarvestingFields);
+            FieldInfo[] harvestingFields = harvestingFieldsType.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+
+            FieldInfo[] gatheredFields;
+
+            string requestedAccMod;
+            while ((requestedAccMod = Console.ReadLine()) != "HARVEST")
+            {
+                switch (requestedAccMod)
+                {
+                    case "private":
+                        gatheredFields = harvestingFields.Where(f => f.IsPrivate).ToArray();
+                        break;
+                    case "protected":
+                        gatheredFields = harvestingFields.Where(f => f.IsFamily).ToArray();
+                        break;
+                    case "public":
+                        gatheredFields = harvestingFields.Where(f => f.IsPublic).ToArray();
+                        break;
+                    case "all":
+                        gatheredFields = harvestingFields.ToArray();
+                        break;
+                    default:
+                        gatheredFields = null;
+                        break;
+                }
+
+                string[] result = gatheredFields
+                    .Select(f => $"{f.Attributes.ToString().ToLower()} {f.FieldType.Name} {f.Name}")
+                    .ToArray();
+                Console.WriteLine(string.Join(Environment.NewLine, result).Replace("family", "protected"));
+            }
+        }
+    }
+}
---
+++ 02BlackBoxInteger/BlackBoxIntegerTests.cs
@@ -0,0 +1,38 @@
+﻿namespace _02BlackBoxInteger
+{
+    using System;
+    using System.Linq;
+    using System.Reflection;
+
+    public class BlackBoxIntegerTests
+    {
+        public static void Main(string[] args)
+        {
+            Type blackBoxType = typeof(BlackBoxInt);
+            BlackBoxInt blackBoxInstance = (BlackBoxInt)Activator.CreateInstance(blackBoxType, true);
+
+            //ConstructorInfo blackBoxCtor = blackBoxType
+            //    .GetConstructor(BindingFlags.Instance | BindingFlags.NonPublic, Type.DefaultBinder, new Type[] { }, null );
+            //blackBoxCtor.Invoke();
+
+            string input;
+            while ((input = Console.ReadLine()) != "END")
+            {
+                var tokens = input.Split('_');
+                string methodName = tokens[0];
+                int value = int.Parse(tokens[1]);
+
+                blackBoxType.GetMethod(methodName, 
+                    BindingFlags.Instance | BindingFlags.NonPublic)
+                    .Invoke(blackBoxInstance, new object[] { value });
+
+                object innerStateValue = blackBoxType
+                    .GetFields(BindingFlags.Instance | BindingFlags.NonPublic)
+                    .First()
+                    .GetValue(blackBoxInstance);
+
+                Console.WriteLine(innerStateValue);
+            }
+        }
+    }
+}
